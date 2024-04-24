bcb621fc920547c82d3c95a89218c71233c9b8fd
AlexProus <167352898+AlexProus@users.noreply.github.com>
2024-04-24 13:00:48 -0500

    Update ShoppingCart.h

--- ShoppingCart.h
+++ ShoppingCart.h
@@ -1,1 +1,30 @@
+#ifndef SHOPPING_CART_H
+#define SHOPPING_CART_H
+
+#include <string>
+#include <vector>
+#include "ItemToPurchase.h" // Ensure this file has include guards too.
+
+class ShoppingCart
+{
+public:
+    ShoppingCart();
+    ShoppingCart(std::string cname, std::string date);
+    std::string GetCustomerName() const;
+    std::string GetDate() const;
+    void AddItem(ItemToPurchase item);
+    void RemoveItem(std::string itemName);
+    void ModifyItem(std::string name, std::string descr, int price, int quantity);
+    int GetNumItemsInCart();
+    int GetCostOfCart();
+    void PrintTotal();
+    void PrintDescriptions();
+
+private:
+    std::string customerName;
+    std::string currentDate;
+    std::vector<ItemToPurchase> cartItems;
+};
+
+#endif // SHOPPING_CART_H
 
